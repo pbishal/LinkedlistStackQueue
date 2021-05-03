ce77fa8c3729d374a4d6d08ceeeb32c0f97dda39
pbishal <pbishal60@gmail.com>
2021-05-03 22:02:25 +0530

    Stack created

--- LinkedlistStackQueue/Program.cs
+++ LinkedlistStackQueue/Program.cs
@@ -11,6 +11,17 @@ namespace LinkedlistStackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linkedlist stack and queue problem");
+            Stack stackOBJ = new Stack();
+
+            //Pushing(adding) elements one by one.
+            //Top elements will be 50.
+            stackOBJ.Push(70);
+            stackOBJ.Push(30);
+            stackOBJ.Push(56);
+
+            //Displaying stack.
+            stackOBJ.DisplayStackElements();
+            Console.ReadLine();
         }
     }
 }
